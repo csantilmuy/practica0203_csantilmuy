86f06209dc17297c6bab707fe8c42ac0356ea3dc
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-21 10:33:52 +0200

    Update Ejercicio_01.py

--- Ejercicio_01.py
+++ Ejercicio_01.py
@@ -2,6 +2,6 @@
 # variable, pregunte al usuario por la contraseña e imprima por pantalla si la
 # contraseña introducida por el usuario coincide con la guardada en la
 # variable sin tener en cuenta mayúsculas y minúsculas.
-contraseña = "Contraseña"
+contraseña = "contraseña"
 peticióncontraseña = input("Escriba su contraseña: ")
 if 
