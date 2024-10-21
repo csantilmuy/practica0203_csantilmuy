18553c35278356ceb11e21b3a1dbf87222e5f407
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-21 10:57:00 +0200

    Create Ejercicio_02.py

---
+++ Ejercicio_02.py
@@ -0,0 +1,5 @@
+# Escribir un programa que pida al usuario dos números y muestre por pantalla
+# su división. Si el divisor es cero el programa debe mostrar un error.
+n1 = float(input("Escriba el primer número: "))
+n2 = float(input("Escriba el segundo número: "))
+if 
