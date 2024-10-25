ec177afb8246cf682154257629ff9c7d7ed6e615
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-25 08:58:27 +0200

    Update Ejercicio_06.py

--- Ejercicio_06.py
+++ Ejercicio_06.py
@@ -1,3 +1,4 @@
 # Escribir un programa que pregunte al usuario su edad y muestre por
 # pantalla todos los años que ha cumplido (desde 1 hasta su edad).
 edad = int(input("Escribe tu edad en años: "))
+for i in range(edad)
