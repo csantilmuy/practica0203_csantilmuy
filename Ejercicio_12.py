32a0189127a4b4af7ebfbb7af7719e040de8fa0d
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-25 08:43:10 +0200

    Update Ejercicio_12.py

--- Ejercicio_12.py
+++ Ejercicio_12.py
@@ -1,3 +1,5 @@
 # Escribir un programa en el que se pregunte al usuario por una frase y una
 # letra, y muestre por pantalla el número de veces que aparece la letra en
 # la frase.
+frase = input("Escribe una frase: ")
+letra = input("Escribe una letra: ")
