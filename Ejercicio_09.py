d17be33dcefba337d9cbdf6e2167bd08151485a7
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-25 08:33:09 +0200

    Update Ejercicio_09.py

--- Ejercicio_09.py
+++ Ejercicio_09.py
@@ -1,3 +1,4 @@
 # Escribir un programa que pida al usuario un número entero y muestre por
 # pantalla un triángulo rectángulo que tenga tantas líneas como el número
 # introducido, como el triángulo de más abajo.
+número = int(input("Escribe un número entero: "))
